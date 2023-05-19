2c587f254ac0e6cc8fbde58272c4246dabbf54aa
shinsanghooon <shinsanghooon@gmail.com>
2023-05-19 23:39:01 +0900

    Initial commit

---
+++ src/main/java/com/example/dogpoll/DogpollApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogpollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogpollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dogpoll/DogpollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogpollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
