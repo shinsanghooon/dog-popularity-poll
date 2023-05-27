003e047ebb8e58e51fefe2a541c0b9ca68d19536
shinsanghooon <shinsanghooon@gmail.com>
2023-05-28 01:37:10 +0900

    settings: redis config 파일 추가

---
+++ src/main/java/com/example/dogpoll/config/RedisCacheConfig.java
@@ -0,0 +1,43 @@
+package com.example.dogpoll.config;
+
+import java.time.Duration;
+import java.util.HashMap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+            .disableCachingNullValues()
+            .entryTtl(Duration.ofSeconds(10))
+            .computePrefixWith(CacheKeyPrefix.simple())
+            .serializeKeysWith(
+                SerializationPair.fromSerializer(new StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+            )
+            ;
+
+        HashMap<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("dogCandidateCache", configuration);
+        configMap.put("allDogCandidatesCache", configuration);
+
+        return RedisCacheManager
+            .RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+            .cacheDefaults(configuration)
+            .withInitialCacheConfigurations(configMap)
+            .build();
+    }
+
+}
